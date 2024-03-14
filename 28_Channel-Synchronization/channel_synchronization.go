3648dec5d9b449b600bb7d2689760ace4ec609d9
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-03-14 18:30:33 +0800

    update to select

---
+++ 28_Channel-Synchronization/channel_synchronization.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Println("Working...")
+
+	time.Sleep(time.Second * 2)
+
+	fmt.Println("Done!")
+
+	done <- true
+}
+
+func main() {
+
+	done := make(chan bool)
+
+	go worker(done)
+
+	/*
+		If you removed the <-done line from this program, the program would exit before the worker even started.
+	*/
+	println(<-done)
+}
