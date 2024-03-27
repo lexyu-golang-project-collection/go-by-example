82ddf67872a2b485d2f525afbf5e81030d3db9e3
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-03-27 18:30:14 +0800

    Update closing_channels.go

--- 33_Closing-Channels/closing_channels.go
+++ 33_Closing-Channels/closing_channels.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
+func main() {
+	jobs := make(chan string, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			time.Sleep(time.Second * 2)
+			if more {
+				fmt.Println("Received Job", j)
+			} else {
+				fmt.Println("Received All Job")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for i := 1; i <= 5; i++ {
+		j := "job-" + strconv.Itoa(i)
+		jobs <- j
+		fmt.Println("Sent Job", j)
+		time.Sleep(time.Second)
+	}
+	close(jobs)
+	fmt.Println("Sent All Job Done")
+
+	<-done
+
+	_, ok := <-jobs
+	fmt.Println("Receive more jobs:", ok)
 }
