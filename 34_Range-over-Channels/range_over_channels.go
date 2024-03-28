4a093ad325f44c0c2aa896155ce3de3541a0e0e9
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-03-28 18:30:42 +0800

    Update range_over_channels.go

--- 34_Range-over-Channels/range_over_channels.go
+++ 34_Range-over-Channels/range_over_channels.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	queue := make(chan string, 2)
+	queue <- "ONE"
+	queue <- "TWO"
+
+	close(queue)
+
+	for e := range queue {
+		fmt.Println("Element:", e)
+	}
 }
