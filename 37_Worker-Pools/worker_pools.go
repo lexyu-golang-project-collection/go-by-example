d258be4f67e22339ed4d09b1ab9e1975392d585e
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-03-31 23:59:49 +0800

    Update worker_pools.go

--- 37_Worker-Pools/worker_pools.go
+++ 37_Worker-Pools/worker_pools.go
@@ -1,8 +1,38 @@
 package main
 
-func worker(id int, jobs <-chan int, results chan<- int) {
+import (
+	"fmt"
+	"time"
+)
 
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for job := range jobs {
+		fmt.Println("worker", id, "started  job", job)
+		time.Sleep(2 * time.Second)
+		fmt.Println("worker", id, "finished  job", job)
+		results <- job * 2
+	}
 }
 
 func main() {
+	const numJobs = 5
+
+	jobs := make(chan int, numJobs)
+	result := make(chan int, numJobs)
+
+	for i := 1; i <= 5; i++ {
+		go worker(i, jobs, result)
+	}
+
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+
+	close(jobs)
+
+	for r := 1; r <= numJobs; r++ {
+		rr := <-result
+		fmt.Println("After Worker:", rr)
+	}
+
 }
