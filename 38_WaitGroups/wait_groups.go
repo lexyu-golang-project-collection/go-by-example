0343acf1345c3f2cfd8bf1575c21d00c10183fb2
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-04-08 18:28:40 +0800

    Update wait_groups.go

--- 38_WaitGroups/wait_groups.go
+++ 38_WaitGroups/wait_groups.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(id int) {
+	fmt.Printf("Worker %d starting\n", id)
+
+	time.Sleep(time.Second)
+
+	fmt.Printf("Worker %d done\n", id)
+}
+
 func main() {
+
+	var wg sync.WaitGroup
+
+	// before 1.22
+	/*
+		for i := 1; i <= 5; i++ {
+			wg.Add(1)
+
+			go func(v int) {
+				defer wg.Done()
+				worker(v)
+			}(i)
+		}
+	*/
+
+	// After 1.22
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			worker(i)
+		}()
+	}
+
+	wg.Wait()
 
 }
