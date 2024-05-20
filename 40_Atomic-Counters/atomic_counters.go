a96261a0fad0d434846b2e93351f60ec179fb762
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-05-21 00:24:11 +0800

    Update atomic_counters.go

--- 40_Atomic-Counters/atomic_counters.go
+++ 40_Atomic-Counters/atomic_counters.go
@@ -12,13 +12,16 @@ func main() {
 	var wg sync.WaitGroup
 
 	for i := 0; i < 50; i++ {
+		wg.Add(1)
 
 		go func() {
 			for i := 0; i < 1000; i++ {
-
+				ops.Add(1)
 			}
+			wg.Done()
 		}()
 	}
 
+	wg.Wait()
 	fmt.Println("ops:", ops.Load())
 }
