3447d0badc79144f8a2b8ff33e798ab214fde6ff
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-06-05 22:01:34 +0800

    update

--- 42_Stateful-Goroutines/stateful_goroutines.go
+++ 42_Stateful-Goroutines/stateful_goroutines.go
@@ -1,5 +1,28 @@
 package main
 
+type readOp struct {
+	key  int
+	resp chan int
+}
+
+type writeOp struct {
+	key  int
+	val  int
+	resp chan bool
+}
+
 func main() {
+
+	var readOps uint64
+	var writeOps uint64
+
+	reads := make(chan readOp)
+	writes := make(chan writeOp)
+
+	// Stateful Goroutine
+
+	// Read Goroutines
+
+	// Write Goroutines
 
 }
--- 43_Sorting/sorting.go
+++ 43_Sorting/sorting.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+)
+
 func main() {
 
+	strs := []string{"c", "z", "a"}
+	slices.Sort(strs)
+	fmt.Println("strs:", strs)
+
+	res := slices.IsSorted(strs)
+	fmt.Println("Sorted:", res)
+
+	slices.Reverse(strs)
+	fmt.Println("strs:", strs)
+	fmt.Println("Sorted:", res)
+
+	nums := []int{100, 5, 2, 9}
+	slices.Sort(nums)
+	fmt.Println("nums:", nums)
+
+	res = slices.IsSorted(nums)
+	fmt.Println("Sorted:", res)
 }
